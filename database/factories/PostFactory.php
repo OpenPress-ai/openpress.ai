57e3fb2ff0c99bdb90732ba07c7fa993d9fbfe76
Christopher David <chris@openagents.com>
2024-10-03 13:58:09 -0500

    Add User model and default fields to PostFactory definition.

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraphs(3, true),
         ];
     }
 }
