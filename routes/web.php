26bd4d55e6f173765e984d54f76fd34a309d7d1a
Christopher David <chris@openagents.com>
2024-10-03 14:12:29 -0500

    Refactor routing in web.php to use controller for dashboard view.

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::middleware('editor')->group(function () {
-        Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-        Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-    });
 });
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware(['auth', 'editor']);
 
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
