478ea7260829f0d2d288a2dfe0d286d9558e3c97
Christopher David <chris@openagents.com>
2024-10-03 14:10:45 -0500

    Refactor web.php to use route grouping for auth middleware.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    Route::middleware('editor')->group(function () {
-        Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-        Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-    });
+Route::middleware(['auth', 'editor'])->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 });
 
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
