677cb475a9ee5d808595eca080192e2b0f649593
Christopher David <chris@openagents.com>
2024-10-03 14:03:40 -0500

    Create file app/Http/Middleware/EditorMiddleware.php

---
+++ app/Http/Middleware/EditorMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EditorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || !$request->user()->is_editor) {
+            abort(403, 'Only editors can access this page.');
+        }
+
+        return $next($request);
+    }
+}
