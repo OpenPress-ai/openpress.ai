ab6aa4b131a82037ffe3df2053cc62ea3e257324
Christopher David <chris@openagents.com>
2024-10-03 13:49:12 -0500

    Create file app/Http/Controllers/PostController.php

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        $user = Auth::user();
+
+        if (!$user->is_editor) {
+            abort(403, 'Only editors can create posts.');
+        }
+
+        $post = $user->posts()->create($request->only(['title', 'content']));
+
+        return redirect()->route('posts.index');
+    }
+}
