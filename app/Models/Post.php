c91127767692968a13db86a2f272158c86c9f64c
Christopher David <chris@openagents.com>
2024-10-03 13:49:03 -0500

    Add fillable properties and user relationship to Post model for database interactions.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
