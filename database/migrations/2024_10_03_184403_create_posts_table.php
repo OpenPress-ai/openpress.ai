5bad4c019bb6c6c37954b0a7d049753bcc434da5
Christopher David <chris@openagents.com>
2024-10-03 13:56:58 -0500

    Add 'user_id', 'title', and 'content' columns to posts table with cascade delete on 'user_id'.

--- database/migrations/2024_10_03_184403_create_posts_table.php
+++ database/migrations/2024_10_03_184403_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
