d988b93086d1d9bf4572866172757f4d2f8b706d
Christopher David <chris@openagents.com>
2024-10-03 13:50:06 -0500

    Refactor UserFactory to improve password handling for security enhancements.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_editor' => false,
         ];
     }
 
@@ -39,6 +40,16 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user is an editor.
+     */
+    public function editor(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_editor' => true,
         ]);
     }
 }
