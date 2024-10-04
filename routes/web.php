3bd05713fc8cc94461b5054b8c1481a4a102ce08
Christopher David <chris@openagents.com>
2024-10-04 17:24:03 -0500

    Refactor dashboard routing to use SiteController and update imports.

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [SiteController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/sites', [SiteController::class, 'store'])->name('sites.store');
+    Route::get('/sites/create', [SiteController::class, 'create'])->name('sites.create');
 });
 
 Route::middleware(['auth', 'editor'])->group(function () {
