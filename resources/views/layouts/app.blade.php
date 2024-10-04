106db58405c395eae6d7e6e796322d8e75e34d96
Christopher David <chris@openagents.com>
2024-10-03 19:35:07 -0500

    Update app.blade.php to enhance font loading performance by preconnecting to fonts.bunny.net.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,13 +16,13 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="dark font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-background">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
         @isset($header)
-        <header class="bg-white dark:bg-gray-800 shadow">
+        <header class="bg-card shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
