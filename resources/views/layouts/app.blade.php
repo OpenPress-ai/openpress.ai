cc96ab8a829d518b3ba9d7642412a2cd4d920098
Christopher David <chris@openagents.com>
2024-10-03 19:38:19 -0500

    layout shaz and typogrpahy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
+<body class="dark font-sans antialiased">
     <div class="min-h-screen bg-background">
         @include('layouts.navigation')
 
