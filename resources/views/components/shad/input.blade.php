8ae7368dbaeae06237cf207670397e5fca4bcf58
Christopher David <chris@openagents.com>
2024-10-03 19:34:25 -0500

    Update input component styles for better theme consistency in `input.blade.php`.

--- resources/views/components/shad/input.blade.php
+++ resources/views/components/shad/input.blade.php
@@ -1,3 +1,3 @@
 @props(['type' => 'text'])
 
-<input type="{{ $type }}" {{ $attributes->merge(['class' => 'flex h-9 w-full rounded-md border border-zinc-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300']) }}>
+<input type="{{ $type }}" {{ $attributes->merge(['class' => 'flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50']) }}>
