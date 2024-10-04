e95564fad37ae115d8b2e47f752312c09258035a
Christopher David <chris@openagents.com>
2024-10-03 20:15:22 -0500

    logout any

--- routes/auth.php
+++ routes/auth.php
@@ -54,6 +54,6 @@ Route::middleware('auth')->group(function () {
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+    Route::any('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
