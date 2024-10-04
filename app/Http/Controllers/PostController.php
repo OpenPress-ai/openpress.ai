7b953dc9ef483cb632dea16ff34bfd388bee54cc
Christopher David <chris@openagents.com>
2024-10-03 22:16:19 -0500

    Add heroPost to PostController index for featuring latest post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->get();
-        return view('posts.index', compact('posts'));
+        $heroPost = Post::latest()->first(); // Get the latest post for the hero card
+        return view('posts.index', compact('posts', 'heroPost'));
     }
 
     public function show(Post $post)
