b383154f411bc6d12b4923a421bbf02003cd2314
Christopher David <chris@openagents.com>
2024-10-05 20:24:08 -0500

    Enhanced CreateForgeSite command to accept domain parameter and integrate ForgeService for site creation.

--- app/Console/Commands/CreateForgeSite.php
+++ app/Console/Commands/CreateForgeSite.php
@@ -2,15 +2,33 @@
 
 namespace App\Console\Commands;
 
+use App\Services\ForgeService;
 use Illuminate\Console\Command;
 
 class CreateForgeSite extends Command
 {
-    protected $signature = 'forge:create-site';
-    protected $description = 'Create an example Forge site';
+    protected $signature = 'forge:create-site {domain}';
+    protected $description = 'Create a Forge site';
 
-    public function handle()
+    public function handle(ForgeService $forgeService)
     {
-        //
+        $serverId = 848551;
+        $domain = $this->argument('domain');
+
+        $siteData = [
+            'domain' => $domain,
+            'project_type' => 'php',
+            'directory' => '/public',
+            'isolated' => false,
+        ];
+
+        $result = $forgeService->createSite($serverId, $siteData);
+
+        if (isset($result['error'])) {
+            $this->error("Failed to create site: " . $result['error']);
+        } else {
+            $this->info("Site created successfully: " . $domain);
+            $this->line("Site ID: " . $result['site']['id']);
+        }
     }
 }
