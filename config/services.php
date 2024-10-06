0927a4731a7db9fb4432652cd003566259c39021
Christopher David <chris@openagents.com>
2024-10-05 20:55:35 -0500

    Updated `services.php` to accommodate new third-party service credentials structure for enhanced security and organization.

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
 
     'forge' => [
         'api_key' => env('FORGE_API_KEY'),
-        'server_id' => env('FORGE_SERVER_ID'),
+        'server_id' => env('FORGE_SERVER_ID', '848551'),
     ],
 
 ];
