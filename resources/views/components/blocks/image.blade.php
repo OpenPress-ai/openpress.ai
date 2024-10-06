756beb4f3f307799c73bced40d482a233a63f73d
Christopher David <chris@openagents.com>
2024-10-06 14:02:40 -0500

    Fix image

--- resources/views/components/blocks/image.blade.php
+++ resources/views/components/blocks/image.blade.php
@@ -1,8 +1,5 @@
-<div class="h-[{{ $block['attributes']['height'] }}px] w-[{{ $block['attributes']['width'] }}px] overflow-hidden">
-    <img
-        src="{{ $block['attributes']['src'] }}"
-        alt="{{ $block['attributes']['alt'] }}"
-        class="{{ $block['attributes']['className'] ?? '' }} w-full h-full rounded-lg object-cover"
-        loading="lazy"
-    >
-</div>
+<img
+    src="{{ $block['attributes']['src'] }}"
+    alt="{{ $block['attributes']['alt'] }}"
+    class="{{ $block['attributes']['className'] ?? '' }} rounded-lg"
+    loading="lazy" />
