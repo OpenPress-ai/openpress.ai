b26c480d253a9572d29db8ee83f932500b3b8724
Christopher David <chris@openagents.com>
2024-10-06 14:03:23 -0500

    hm

--- resources/views/components/blocks/image.blade.php
+++ resources/views/components/blocks/image.blade.php
@@ -1,5 +1,5 @@
 <img
     src="{{ $block['attributes']['src'] }}"
     alt="{{ $block['attributes']['alt'] }}"
-    class="{{ $block['attributes']['className'] ?? '' }} rounded-lg"
+    class="{{ $block['attributes']['className'] ?? '' }}"
     loading="lazy" />
