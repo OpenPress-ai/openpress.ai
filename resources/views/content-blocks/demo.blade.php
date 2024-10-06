9ef9fe6f2482fd4a4140dd6bb84c70ddb6fa4df8
Christopher David <chris@openagents.com>
2024-10-06 14:30:38 -0500

    Add theme support to block components in demo.blade.php

--- resources/views/content-blocks/demo.blade.php
+++ resources/views/content-blocks/demo.blade.php
@@ -4,7 +4,7 @@
             <div class="p-6">
                 <h1 class="text-3xl font-bold mb-4">{{ $page['title'] }}</h1>
                 @foreach($page['blocks'] as $block)
-                    @include('components.blocks.' . strtolower($block['type']), ['block' => $block])
+                    @include('components.blocks.' . strtolower($block['type']), ['block' => $block, 'theme' => $page['theme']])
                 @endforeach
             </div>
         </div>
