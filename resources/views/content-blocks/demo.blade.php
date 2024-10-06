833d08013167c70113c0589f3fcc3f500424abb8
Christopher David <chris@openagents.com>
2024-10-06 14:48:08 -0500

    Refactor demo.blade.php to simplify structure by removing nested divs and directly iterating over blocks.

--- resources/views/content-blocks/demo.blade.php
+++ resources/views/content-blocks/demo.blade.php
@@ -1,12 +1,5 @@
 <x-blocks-layout :theme="$page['theme']">
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-6">
-                <h1 class="text-3xl font-bold mb-4">{{ $page['title'] }}</h1>
-                @foreach($page['blocks'] as $block)
-                    @include('components.blocks.' . strtolower($block['type']), ['block' => $block, 'theme' => $page['theme']])
-                @endforeach
-            </div>
-        </div>
-    </div>
+    @foreach($page['blocks'] as $block)
+        @include('components.blocks.' . strtolower($block['type']), ['block' => $block, 'theme' => $page['theme']])
+    @endforeach
 </x-blocks-layout>
