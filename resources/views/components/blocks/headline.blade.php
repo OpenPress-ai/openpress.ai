99a528b0bd72474d3da2e87a0d5f129fc0f35dc6
Christopher David <chris@openagents.com>
2024-10-06 13:50:26 -0500

    Create file resources/views/components/blocks/headline.blade.php

---
+++ resources/views/components/blocks/headline.blade.php
@@ -0,0 +1,3 @@
+<{{ $block['attributes']['level'] }} class="text-{{ $block['attributes']['level'] === 'h1' ? '4xl' : '2xl' }} font-bold mb-4">
+    {{ $block['attributes']['content'] }}
+</{{ $block['attributes']['level'] }}>
