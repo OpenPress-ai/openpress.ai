efda9b928c8c936e70a27b3a16887e9f0f7a7c26
Christopher David <chris@openagents.com>
2024-10-06 13:53:18 -0500

    Refactor `headline.blade.php` to include dynamic class handling and text styles based on headline level using `@switch` directive for enhanced readability and style customization.

--- resources/views/components/blocks/headline.blade.php
+++ resources/views/components/blocks/headline.blade.php
@@ -1,3 +1,16 @@
-<{{ $block['attributes']['level'] }} class="text-{{ $block['attributes']['level'] === 'h1' ? '4xl' : '2xl' }} font-bold mb-4">
+<{{ $block['attributes']['level'] }} class="scroll-m-20 {{ $block['attributes']['className'] ?? '' }}
+    @switch($block['attributes']['level'])
+        @case('h1')
+            text-4xl font-extrabold tracking-tight lg:text-5xl
+            @break
+        @case('h2')
+            text-3xl font-semibold tracking-tight
+            @break
+        @case('h3')
+            text-2xl font-semibold tracking-tight
+            @break
+        @default
+            text-xl font-semibold tracking-tight
+    @endswitch">
     {{ $block['attributes']['content'] }}
 </{{ $block['attributes']['level'] }}>
