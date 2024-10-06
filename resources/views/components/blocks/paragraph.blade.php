099df424841a8e26b1aba74a4f6a1f10715750b5
Christopher David <chris@openagents.com>
2024-10-06 16:00:00 -0500

    Refactor paragraph.blade.php to wrap content in <div>, split into multiple <p> tags using newline, enhancing readability and structure.

--- resources/views/components/blocks/paragraph.blade.php
+++ resources/views/components/blocks/paragraph.blade.php
@@ -1,3 +1,5 @@
-<p class="{{ $block['attributes']['className'] ?? '' }}" style="{{ isset($block['attributes']['style']) ? implode('; ', array_map(function($key, $value) { return $key . ': ' . $value; }, array_keys($block['attributes']['style']), $block['attributes']['style'])) : '' }}">
-    {{ $block['attributes']['content'] }}
-</p>
+<div class="{{ $block['attributes']['className'] ?? '' }}" style="{{ isset($block['attributes']['style']) ? implode('; ', array_map(function($key, $value) { return $key . ': ' . $value; }, array_keys($block['attributes']['style']), $block['attributes']['style'])) : '' }}">
+    @foreach(explode("\n", $block['attributes']['content']) as $paragraph)
+        <p class="mb-4">{{ $paragraph }}</p>
+    @endforeach
+</div>
