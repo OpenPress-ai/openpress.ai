03d41b7f4fa37190eed9fe006557b4a6604c53e0
Christopher David <chris@openagents.com>
2024-10-06 14:58:04 -0500

    Refactor container class assignment in demo.blade.php to use implode for better array handling.

--- resources/views/content-blocks/demo.blade.php
+++ resources/views/content-blocks/demo.blade.php
@@ -1,5 +1,5 @@
 <x-blocks-layout :theme="$page['theme']">
-    <div class="{{ $page['containerClasses'] ?? '' }}" style="background-color: {{ $page['theme']['background-color'] }}; color: {{ $page['theme']['color'] }};">
+    <div class="{{ implode(' ', $page['containerClasses']) }}" style="background-color: {{ $page['theme']['background-color'] }}; color: {{ $page['theme']['color'] }};">
         @foreach($page['blocks'] as $block)
             @include('components.blocks.' . strtolower($block['type']), ['block' => $block, 'theme' => $page['theme']])
         @endforeach
