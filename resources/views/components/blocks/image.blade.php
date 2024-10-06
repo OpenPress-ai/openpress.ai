59c2f00297a5f6dfd1aab1ad4513d35f108728d4
Christopher David <chris@openagents.com>
2024-10-06 15:14:36 -0500

    logo

--- resources/views/components/blocks/image.blade.php
+++ resources/views/components/blocks/image.blade.php
@@ -2,4 +2,5 @@
     src="{{ $block['attributes']['src'] }}"
     alt="{{ $block['attributes']['alt'] }}"
     class="{{ $block['attributes']['className'] ?? '' }}"
+    width="{{ $block['attributes']['width'] }}"
     loading="lazy" />
