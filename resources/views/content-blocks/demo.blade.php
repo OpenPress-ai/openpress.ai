876277e1faa5aae5efde3f40ae6eda7f21e57e6d
Christopher David <chris@openagents.com>
2024-10-06 14:23:18 -0500

    Refactor `demo.blade.php` to remove inline text color style from main content div for cleaner CSS management.

--- resources/views/content-blocks/demo.blade.php
+++ resources/views/content-blocks/demo.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-6">
                     <h1 class="text-3xl font-bold mb-4">{{ $page['title'] }}</h1>
                     @foreach($page['blocks'] as $block)
                         @include('components.blocks.' . strtolower($block['type']), ['block' => $block])
