e5a3f22ef8fa053bc3342e8f21a13b174886e3a0
Christopher David <chris@openagents.com>
2024-10-05 20:21:38 -0500

    start CreateForgeSite

---
+++ app/Console/Commands/CreateForgeSite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateForgeSite extends Command
+{
+    protected $signature = 'forge:create-site';
+    protected $description = 'Create an example Forge site';
+
+    public function handle()
+    {
+        //
+    }
+}
