550cf0894a856190ae45bb9ea06a1c1717259b66
Christopher David <chris@openagents.com>
2024-10-06 13:53:23 -0500

    Create file resources/views/components/blocks/image.blade.php

---
+++ resources/views/components/blocks/image.blade.php
@@ -0,0 +1,8 @@
+<img
+    src="{{ $block['attributes']['src'] }}"
+    alt="{{ $block['attributes']['alt'] }}"
+    width="{{ $block['attributes']['width'] }}"
+    height="{{ $block['attributes']['height'] }}"
+    class="{{ $block['attributes']['className'] ?? '' }} rounded-lg object-cover"
+    loading="lazy"
+>
