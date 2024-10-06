fb8fa287b8e1fada3254ef17f2289582d26f23ed
Christopher David <chris@openagents.com>
2024-10-06 14:29:31 -0500

    Add theme attribute to x-blocks-layout in demo.blade.php for dynamic theming support.

--- resources/views/content-blocks/demo.blade.php
+++ resources/views/content-blocks/demo.blade.php
@@ -1,10 +1,10 @@
-<x-blocks-layout>
+<x-blocks-layout :theme="$page['theme']">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6">
                 <h1 class="text-3xl font-bold mb-4">{{ $page['title'] }}</h1>
                 @foreach($page['blocks'] as $block)
-                @include('components.blocks.' . strtolower($block['type']), ['block' => $block])
+                    @include('components.blocks.' . strtolower($block['type']), ['block' => $block])
                 @endforeach
             </div>
         </div>
