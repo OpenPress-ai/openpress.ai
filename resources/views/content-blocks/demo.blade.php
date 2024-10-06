95c17008fc11b0a26fd8573f0080dbc850ed7add
Christopher David <chris@openagents.com>
2024-10-06 13:53:51 -0500

    Refactor demo.blade.php to use x-app-layout and improve styling and layout structure.

--- resources/views/content-blocks/demo.blade.php
+++ resources/views/content-blocks/demo.blade.php
@@ -1,10 +1,14 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container mx-auto px-4">
-        <h1 class="text-3xl font-bold mb-4">{{ $page['title'] }}</h1>
-        @foreach($page['blocks'] as $block)
-            @include('components.blocks.' . strtolower($block['type']), ['block' => $block])
-        @endforeach
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h1 class="text-3xl font-bold mb-4">{{ $page['title'] }}</h1>
+                    @foreach($page['blocks'] as $block)
+                        @include('components.blocks.' . strtolower($block['type']), ['block' => $block])
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
-@endsection
+</x-app-layout>
